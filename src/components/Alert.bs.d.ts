c54127961655e5802711e55f769bd8acfffe4821
hamza0867 <hamza0867@gmail.com>
2020-01-06 13:22:54 +0100

    Remove src/components/Alert.bs.d.ts file since it is not needed for docz to work

--- src/components/Alert.bs.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export declare const Make: React.FC<{ readonly text: string }>;
-export declare const Alert: React.FC<{ readonly text: string }>;
-export declare const alert: React.FC<{ readonly text: string }>;
